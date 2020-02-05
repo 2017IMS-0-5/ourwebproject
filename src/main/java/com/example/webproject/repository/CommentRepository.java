f1db426ca55366c0f70ab35e08a4a57230db7588
BNULXR <201711260115@mail.bnu.edu.cn>
2020-02-05 13:42:49 +0800

    更新了Comment的方法

--- src/main/java/com/example/webproject/repository/CommentRepository.java
+++ src/main/java/com/example/webproject/repository/CommentRepository.java
@@ -1,16 +1,31 @@
 package com.example.webproject.repository;
 
 import com.example.webproject.entity.Comment;
+import com.example.webproject.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Date;
+import java.sql.Timestamp;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     /**
-     * 根据用户姓名分页查询用户列表
+     * 根据用户账号分页查询评论列表
      * @param userAccount
      * @param pageable
      * @return
      */
     Page<Comment> findByUserAccount(String userAccount,Pageable pageable);
+
+    /**
+     *根据日期分页查询评论列表
+     * @param date
+     * @return
+     */
+    @Query(name="findByDate",nativeQuery = true,value =
+            "select * from comment where left(createTime,10)=:date")
+    Page<Comment> findByDate(@Param("date") Date date,Pageable pageable);
 }
--- src/main/java/com/example/webproject/service/CommentServiceImpl.java
+++ src/main/java/com/example/webproject/service/CommentServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,6 +36,11 @@ public class CommentServiceImpl implements CommentService{
     @Override
     public Comment addComment(Comment comment){
         return commentRepository.save(comment);
+    }
+
+    @Override
+    public Page<Comment> getByDate(Date date, Pageable pageable){
+        return commentRepository.findByDate(date,pageable);
     }
 
 }
